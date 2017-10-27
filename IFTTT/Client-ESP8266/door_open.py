bc216d093feedbee12b28fe92fd02b312e3e3177
Tiago Silva <tiagospies@gmail.com>
2017-10-26 22:51:00 -0200

    REF - door_open.py funcional.

--- IFTTT/Client-ESP8266/door_open.py
+++ IFTTT/Client-ESP8266/door_open.py
@@ -7,7 +7,7 @@ import urequests
 import ujson
 
 #GPIO of red-switch
-doorSensor = Pin(0, Pin.IN)
+doorSensor = Pin(0, Pin.IN, machine.Pin.PULL_UP)
 
 IFTTT_SERVER = "http://maker.ifttt.com/trigger/opendoor/with/key/"
 IFTTT_KEY = "cAa_G_-pTA-FDANT-d6QF2"
@@ -25,7 +25,7 @@ def do_connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('tiago', 'tiagoecamila')
+        sta_if.connect('Felipe', 'tiagoeca')
         while not sta_if.isconnected():
             machine.idle() 
     print('network config:', sta_if.ifconfig())
@@ -33,19 +33,10 @@ def do_connect():
 
 do_connect()
 
-def send_status_open():
-    payload = "{\n\t\"value1\":\"Aberta\"\n}"
+def send_status_open(p):
+    print('pin change', p)
+    payload = "{\n\t\"value1\":\"AbertaHW\"\n}"
     response = urequests.post(IFTTT_URL_CALL, headers = HEADER_REQUEST, data = payload)
     response.close()
 
-def send_status_close():
-    payload = "{\n\t\"value1\":\"Fechada\"\n}"
-    response = urequests.post(IFTTT_URL_CALL, headers = HEADER_REQUEST, data = payload)
-    response.close()
-
-send_status_open()
-time.sleep(2)
-send_status_close()
-
-#doorSensor.irq(trigger=Pin.IRQ_FALLING, handler=send_status_close)
-#doorSensor.irq(trigger=Pin.IRQ_RISING, handler=send_status_open)
+doorSensor.irq(trigger=Pin.IRQ_RISING, handler=send_status_open)
